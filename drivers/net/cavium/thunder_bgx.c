bd30e1c8337218eb36065a86fae2ef88604af033
Chandrakala Chavva <cchavva@caviumnetworks.com>
2017-07-07 21:47:34 -0700

    netdev:octeontx: Program BGXX_SPUX_AD_ADV CSR properly.
    Signed-off-by: Chandrakala Chavva <cchavva@caviumnetworks.com>

--- drivers/net/cavium/thunder_bgx.c
+++ drivers/net/cavium/thunder_bgx.c
@@ -497,13 +497,14 @@ static int bgx_lmac_xaui_init(struct bgx *bgx, int lmacid, int lmac_type)
 	bgx_reg_write(bgx, lmacid, BGX_SPUX_AN_CONTROL, cfg);
 
 	cfg = bgx_reg_read(bgx, lmacid, BGX_SPUX_AN_ADV);
+	/* Clear all KR bits, configure according to the mode */
+	cfg &= ~((0xfULL << 22) || (1ULL << 12));
 	if (lmac->qlm_mode == QLM_MODE_10G_KR)
 		cfg |= (1 << 23);
 	else if (lmac->qlm_mode == QLM_MODE_40G_KR4)
 		cfg |= (1 << 24);
 	else
 		cfg &= ~((1 << 23) | (1 << 24));
-	cfg = cfg & (~((1ULL << 25) | (1ULL << 22) | (1ULL << 12)));
 	bgx_reg_write(bgx, lmacid, BGX_SPUX_AN_ADV, cfg);
 
 	cfg = bgx_reg_read(bgx, 0, BGX_SPU_DBG_CONTROL);
