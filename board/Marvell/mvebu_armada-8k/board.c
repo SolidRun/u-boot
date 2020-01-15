abd9f07ad67f5495fa6e38ff56129c83d4b1ac01
Konstantin Porotchkin <kostap@marvell.com>
2020-01-15 19:35:55 +0200

    arch/arm: mvebu: pci-bootcmd support on PCIe EP platfrorms
    Enable pci-bootcmd memory buffer and IOMUX support on
    PCIe EP platforms.
    
    Change-Id: I02c85c70bf218d5497c459f56d12ee2936be9641
    Signed-off-by: Konstantin Porotchkin <kostap@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/21666
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Stefan Chulski <stefanc@marvell.com>

--- board/Marvell/mvebu_armada-8k/board.c
+++ board/Marvell/mvebu_armada-8k/board.c
@@ -4,6 +4,7 @@
  */
 
 #include <common.h>
+#include <console.h>
 #include <dm.h>
 #include <i2c.h>
 #include <asm/io.h>
@@ -21,6 +22,8 @@
 #define CP_USB20_TX_OUT_AMPL_VALUE	(0x3 << 20)
 
 DECLARE_GLOBAL_DATA_PTR;
+
+#define BOOTCMD_NAME	"pci-bootcmd"
 
 int __soc_early_init_f(void)
 {
@@ -58,6 +61,53 @@ int board_init(void)
 	return 0;
 }
 
+#if (CONFIG_IS_ENABLED(OCTEONTX_SERIAL_BOOTCMD) ||	\
+	CONFIG_IS_ENABLED(OCTEONTX_SERIAL_PCIE_CONSOLE)) &&	\
+	!CONFIG_IS_ENABLED(CONSOLE_MUX)
+# error CONFIG_CONSOLE_MUX must be enabled!
+#endif
+
+#if CONFIG_IS_ENABLED(OCTEONTX_SERIAL_BOOTCMD)
+static int init_bootcmd_console(void)
+{
+	int ret = 0;
+	char *stdinname = env_get("stdin");
+	struct udevice *bootcmd_dev = NULL;
+	bool stdin_set;
+	char iomux_name[128];
+
+	debug("%s: stdin before: %s\n", __func__,
+	      stdinname ? stdinname : "NONE");
+	if (!stdinname) {
+		env_set("stdin", "serial");
+		stdinname = env_get("stdin");
+	}
+	stdin_set = !!strstr(stdinname, BOOTCMD_NAME);
+	ret = uclass_get_device_by_driver(UCLASS_SERIAL,
+					  DM_GET_DRIVER(octeontx_bootcmd),
+					  &bootcmd_dev);
+	if (ret) {
+		pr_err("%s: Error getting %s serial class\n", __func__,
+		       BOOTCMD_NAME);
+	} else if (bootcmd_dev) {
+		if (stdin_set)
+			strncpy(iomux_name, stdinname, sizeof(iomux_name));
+		else
+			snprintf(iomux_name, sizeof(iomux_name), "%s,%s",
+				 stdinname, bootcmd_dev->name);
+		ret = iomux_doenv(stdin, iomux_name);
+		if (ret)
+			pr_err("%s: Error %d enabling the PCI bootcmd input console \"%s\"\n",
+			       __func__, ret, iomux_name);
+		if (!stdin_set)
+			env_set("stdin", iomux_name);
+	}
+	debug("%s: Set iomux and stdin to %s (ret: %d)\n",
+	      __func__, iomux_name, ret);
+	return ret;
+}
+#endif
+
 int board_late_init(void)
 {
 	/* Pre-configure the USB ports (overcurrent, VBus) */
@@ -74,5 +124,9 @@ int board_late_init(void)
 					CP_USB20_TX_OUT_AMPL_VALUE);
 	}
 
+#if CONFIG_IS_ENABLED(OCTEONTX_SERIAL_BOOTCMD)
+	if (init_bootcmd_console())
+		printf("Failed to init bootcmd input\n");
+#endif
 	return 0;
 }
