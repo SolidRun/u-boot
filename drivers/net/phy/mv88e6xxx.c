73d009c0e5f60ce555976e8d63716e9f07a11511
Kostya Porotchkin <kostap@marvell.com>
2020-04-22 08:34:59 -0700

    Revert "drivers: mv88e6xxx add switch id 6193x"
    This reverts commit b7a9f6b2d768cbf89d64a3a2246cbf0c23c5daa7.
    
    Change-Id: I0487321f3ed7cefe57be6ab832ab7f4a107321f2
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/u-boot/+/27343
    Reviewed-by: Kostya Porotchkin <kostap@marvell.com>
    Tested-by: Kostya Porotchkin <kostap@marvell.com>

--- drivers/net/phy/mv88e6xxx.c
+++ drivers/net/phy/mv88e6xxx.c
@@ -239,8 +239,7 @@ void mv88e6xxx_display_switch_info(struct mv88e6xxx_dev *dev)
 	if (product_num == PORT_SWITCH_ID_PROD_NUM_6390 ||
 	    product_num == PORT_SWITCH_ID_PROD_NUM_6390X ||
 	    product_num == PORT_SWITCH_ID_PROD_NUM_6290 ||
-	    product_num == PORT_SWITCH_ID_PROD_NUM_6190 ||
-	    product_num == PORT_SWITCH_ID_PROD_NUM_6193X) {
+	    product_num == PORT_SWITCH_ID_PROD_NUM_6190) {
 		printf("Switch    : SOHO\n");
 		printf("Series    : Peridot\n");
 		printf("Product # : %X\n", product_num);
@@ -321,10 +320,9 @@ int mv88e6xxx_get_switch_id(struct mv88e6xxx_dev *dev)
 
 	product_num = id >> 4;
 	if ((product_num == PORT_SWITCH_ID_PROD_NUM_6190) ||
-	   (product_num == PORT_SWITCH_ID_PROD_NUM_6193X) ||
-	   (product_num == PORT_SWITCH_ID_PROD_NUM_6290)  ||
-	   (product_num == PORT_SWITCH_ID_PROD_NUM_6390)  ||
-	   (product_num == PORT_SWITCH_ID_PROD_NUM_6390X)) {
+	    (product_num == PORT_SWITCH_ID_PROD_NUM_6290) ||
+	    (product_num == PORT_SWITCH_ID_PROD_NUM_6390) ||
+	    (product_num == PORT_SWITCH_ID_PROD_NUM_6390X)) {
 		/* Peridot switch port device address starts from 0 */
 		REG_PORT_BASE = REG_PORT_BASE_PERIDOT;
 		return id;
@@ -588,7 +586,7 @@ static int do_sw(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		       argv[1]);
 
 	default:
-		return CMD_RET_USAGE;
+		break;
 	}
 	return 0;
 }
