3653827c1f8d67d4c46f87013a9ab0a954f97aa0
Chandrakala Chavva <Chandrakala.Chavva@cavium.com>
2020-07-23 13:35:24 -0700

    Revert "t96: Fix Klockwork with NULL derefence issue"
    This reverts commit 053d92ca72d463400c813179c60cd5ecb9c87707.
    
    Change-Id: Ia15fed5486ca5e125f0f560403ea22d300d92fcb
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/u-boot/+/32595
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- board/Marvell/octeontx2/board.c
+++ board/Marvell/octeontx2/board.c
@@ -353,8 +353,8 @@ int board_late_init(void)
 	str = fdt_get_board_revision();
 	if (str) {
 		snprintf(boardrev, sizeof(boardrev), "%s", str);
-		str = env_get("boardrev");
-		if (strcmp(boardrev, str))
+		if (env_get("boardrev") &&
+		    strcmp(boardrev, env_get("boardrev")))
 			save_env = true;
 		env_set("boardrev", boardrev);
 	}
@@ -362,8 +362,8 @@ int board_late_init(void)
 	str = fdt_get_board_serial();
 	if (str) {
 		snprintf(boardserial, sizeof(boardserial), "%s", str);
-		str = env_get("serial#");
-		if (strcmp(boardserial, str))
+		if (env_get("serial#") &&
+		    strcmp(boardserial, env_get("serial#")))
 			save_env = true;
 		env_set("serial#", boardserial);
 	}
