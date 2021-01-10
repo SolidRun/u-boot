f21beaaa7d42a797244742a5596883aaaf3588d9
Narinder Dhillon <narinder.dhillon@cavium.com>
2021-01-09 21:06:35 -0800

    efi: Changes to fix compiler errors on non-10k platforms
    This commit fixes compiler errors generated in T96 & Loki
    build due to changes made in CN10K platforms. These changes
    are related to access to secure flash from U-Boot.
    
    Signed-off-by: Narinder Dhillon <narinder.dhillon@cavium.com>
    
    Change-Id: Id5e886bab1064b2f7d6ad8971ad42e69ee28b4ac
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/u-boot/+/43343
    Reviewed-by: Suneel Garapati <sgarapati@caviumnetworks.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- lib/efi_loader/efi_setup.c
+++ lib/efi_loader/efi_setup.c
@@ -348,10 +348,11 @@ efi_status_t efi_init_obj_list(void)
 	if (ret != EFI_SUCCESS)
 		goto out;
 
+#ifdef CONFIG_EFI_SPI_NOR_FLASH_PROTOCOL
 	ret = efi_spinor_protocol_register();
 	if (ret != EFI_SUCCESS)
 		goto out;
-
+#endif
 	/* Initialize EFI runtime services */
 	ret = efi_reset_system_init();
 	if (ret != EFI_SUCCESS)
