b283308150dc68c79b509d3a20ae429147822f7c
Narinder Dhillon <ndhillon@marvell.com>
2021-07-10 17:36:58 -0700

    efi: Disable watchdog
    This commit disables watchdog before jumping to EFI
    application. As EFI applications are not aware of this
    watchdog and don't pet it, causing resets.
    
    Signed-off-by: Narinder Dhillon <ndhillon@marvell.com>
    
    Change-Id: I2fb30d32922241ec7b5710bb75ca29a95386a0c5
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/u-boot/+/56052
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- cmd/bootefi.c
+++ cmd/bootefi.c
@@ -26,6 +26,7 @@
 #include <memalign.h>
 #include <asm-generic/sections.h>
 #include <linux/linkage.h>
+#include <wdt.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -691,6 +692,10 @@ static int do_bootefi(struct cmd_tbl *cmdtp, int flag, int argc,
 	else if (ret != EFI_SUCCESS)
 		return CMD_RET_FAILURE;
 
+	/* Disable watch dog */
+#if CONFIG_IS_ENABLED(WDT)
+	wdt_stop(gd->watchdog_dev);
+#endif
 	if (IS_ENABLED(CONFIG_CMD_BOOTEFI_BOOTMGR)) {
 		if (!strcmp(argv[1], "bootmgr"))
 			return do_efibootmgr();
