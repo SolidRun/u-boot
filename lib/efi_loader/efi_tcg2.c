04708428ed3bb12fe933589546c85664fadfa793
Ilias Apalodimas <ilias.apalodimas@linaro.org>
2021-11-18 10:13:42 +0200

    efi_loader: startup the tpm device when installing the protocol
    Due to U-Boot's lazy binding mentality the TPM is probed but not properly
    initialized.  The user can startup the device from the command line
    e.g 'tpm2 startup TPM2_SU_CLEAR'.  However we can initialize the TPM during
    the TCG protocol installation,  which is easier to use overall.
    
    Change-Id: I976a997b12d9ae389dedc868752e4edf841ce420
    Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
    Reviewed-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/u-boot/+/82001
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- lib/efi_loader/efi_tcg2.c
+++ lib/efi_loader/efi_tcg2.c
@@ -15,6 +15,7 @@
 #include <log.h>
 #include <version.h>
 #include <tpm-v2.h>
+#include <tpm_api.h>
 #include <u-boot/sha1.h>
 #include <u-boot/sha256.h>
 #include <u-boot/sha512.h>
@@ -1167,11 +1168,19 @@ efi_status_t efi_tcg2_register(void)
 {
 	efi_status_t ret = EFI_SUCCESS;
 	struct udevice *dev;
+	int err;
 
 	ret = platform_get_tpm2_device(&dev);
 	if (ret != EFI_SUCCESS) {
 		log_warning("Unable to find TPMv2 device\n");
 		return EFI_SUCCESS;
+	}
+
+	/* initialize the TPM as early as possible. */
+	err = tpm_startup(dev, TPM_ST_CLEAR);
+	if (err) {
+		log_err("TPM startup failed\n");
+		goto fail;
 	}
 
 	ret = efi_init_event_log();
