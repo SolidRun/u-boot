ac3f6886661396a152e9baaecfc4cb2d95f89c36
Pragnesh Patel <pragneshp@marvell.com>
2022-09-22 00:13:18 -0700

    board: marvell: otx2: Update boot status info in RST_COLD_DATAX(2) reg
    U-boot will update the boot status in the RST_COLD_DATAX(2) reg
    MCP will read this register and report the same to BMC
    
    Signed-off-by: Pragnesh Patel <pragneshp@marvell.com>
    Change-Id: I3632ed6fc222fbdaaa8923434309fecb03234248
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/u-boot/+/86917
    Reviewed-by: Selvam Venkatachalam <selvamv@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- arch/arm/include/asm/arch-octeontx2/board.h
+++ arch/arm/include/asm/arch-octeontx2/board.h
@@ -178,6 +178,80 @@ union rst_boot {
 	} cnf95xx;
 };
 
+/**
+ * Register (RSL) rst_cold_data#
+ *
+ * RST Cold Reset Data Registers
+ * This register is not accessible through ROM scripts; see SCR_WRITE32_S[ADDR].
+ */
+union rst_cold_datax
+{
+	uint64_t u;
+	struct rst_cold_datax_s
+	{
+		uint64_t data                       : 64;
+	} s;
+
+	struct rst_cold_datax_cn9
+	{
+		uint64_t data                       : 64;
+	} cn9;
+};
+
+/**
+ * Structure rst_cold_data2_s
+ *
+ * RST Cold Data 2 Field Structure
+ * This structure specifies the bit flags used for communication between SCP BL1 and BDK software.
+ * This structure also specifies boot stage info
+ *      Every boot stage has 3 bits to report boot stage info,
+ *      Bit 0 - Boot Status
+ *              0 = not to this stage yet
+ *              1 = successfully boot to the stage
+ *      Bit [1: 2] - Boot Error
+ *              enum boot_error
+ *              {
+ *                  BOOT_NEXT_STAGE_SUCCESS = 0x0,
+ *                  BOOT_NEXT_STAGE_ERROR,
+ *              };
+ */
+union rst_cold_data2_s
+{
+	uint64_t u;
+	struct rst_cold_data2_s_s
+	{
+		uint64_t fail_safe_boot_delaying : 1;/**< [  0:  0] Check if firmware has a delay for interaction with user or other valid reasons. */
+		uint64_t reserved_1            : 1;
+		uint64_t scp_to_bdk_vrm_problem : 1; /**< [  2:  2] SCP sets to tell BDK to not proceed if the AVS bus is not working. */
+		uint64_t bdk_to_scp_vrm_problem : 1; /**< [  3:  3] BDK sets to tell SCP to not proceed if the PMBus configuration failed. */
+		uint64_t cust                  : 8;  /**< [ 11:  4] For customer use. */
+		uint64_t reserved_12_42        : 31;
+		uint64_t ebf_boot_status : 1;  /**< [  43:  43] EBF boot status */
+		uint64_t ebf_boot_error : 2;  /**< [  45:  44] EBF boot stage error */
+		uint64_t atf_bl1_boot_status : 1;  /**< [  46:  46] ATF bl1 boot status */
+		uint64_t atf_bl1_boot_error : 2;  /**< [  48:  47] ATF bl1 boot stage error */
+		uint64_t atf_bl2_boot_status : 1;  /**< [  49:  49] ATF bl2 boot status */
+		uint64_t atf_bl2_boot_error : 2;  /**< [  51:  50] ATF bl2 boot stage error */
+		uint64_t atf_bl31_boot_status : 1;  /**< [  52:  52] ATF bl31 boot status */
+		uint64_t atf_bl31_boot_error : 2;  /**< [  54:  53] ATF bl31 boot stage error */
+		uint64_t atf_bl32_boot_status : 1;  /**< [  55:  55] ATF bl32 boot status */
+		uint64_t atf_bl32_boot_error : 2;  /**< [  57:  56] ATF bl32 boot stage error */
+		uint64_t uboot_boot_status : 1;  /**< [  58:  58] U-boot boot status */
+		uint64_t uboot_boot_error : 2;  /**< [  60:  59] U-boot boot stage error */
+		uint64_t linux_boot_status : 1;  /**< [  61:  61] Linux boot status */
+		uint64_t linux_boot_error : 2;  /**< [  63:  62] Linux boot stage error */
+	} s;
+	/* struct rst_cold_data2_s_s cn; */
+};
+
+static inline uint64_t RST_COLD_DATAX(uint64_t a)
+	__attribute__ ((pure, always_inline));
+static inline uint64_t RST_COLD_DATAX(uint64_t a)
+{
+	if (a<=5)
+		return 0x87e0060017c0ULL + 8ULL * ((a) & 0x7);
+}
+
 extern unsigned long fdt_base_addr;
 
 /** Function definitions */
