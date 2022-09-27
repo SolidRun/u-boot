7eb6d851e82ba7f8882dab791b755e690cf44d56
Yazan Shhady <yazan.shhady@solid-run.com>
2022-09-27 23:21:45 +0300

    change board name to rzg2lc-solidrun

--- include/configs/rzg2lc-solidrun.h
+++ include/configs/rzg2lc-solidrun.h
@@ -3,8 +3,8 @@
  * Copyright (C) 2015 Renesas Electronics Corporation
  */
 
-#ifndef __SMARC_RZG2L_H
-#define __SMARC_RZG2L_H
+#ifndef __RZG2LC_SOLIDRUN_H
+#define __RZG2LC_SOLIDRUN_H
 
 #include <asm/arch/rmobile.h>
 
@@ -66,9 +66,10 @@
 	"bootimage=unzip 0x4A080000 0x48080000; booti 0x48080000 - 0x48000000 \0" \
 	"emmcload=ext4load mmc 0:1 0x4A080000 boot/Image.gz;ext4load mmc 0:1 0x48000000 boot/r9a07g044l-smarc-rzg2l.dtb;run prodemmcbootargs \0" \
 	"sd1load=ext4load mmc 1:1 0x4A080000 boot/Image.gz;ext4load mmc 1:1 0x48000000 boot/r9a07g044l-smarc-rzg2l.dtb;run prodsdbootargs \0" \
+	"sd_boot=mmc dev 0;fatload mmc 0:1 0x48080000 Image; fatload mmc 0:1 0x48000000 r9a07g044c2-smarc.dtb;booti 0x48080000 - 0x48000000" \
 	"bootcmd_check=if mmc dev 1; then run sd1load; else run emmcload; fi \0"
 
-#define CONFIG_BOOTCOMMAND	"env default -a;run bootcmd_check;run bootimage;" \
+#define CONFIG_BOOTCOMMAND	"env default -a;run bootcmd_check;run bootimage;echo test;" \
 "setenv bootargs 'root=/dev/mmcblk0p2 rootwait' ;echo 'rzg2lc-solidrun';" \
 "mmc dev 0;fatload mmc 0:1 0x48080000 Image; fatload mmc 0:1 0x48000000 r9a07g044c2-smarc.dtb;" \
 "booti 0x48080000 - 0x48000000"
@@ -77,4 +78,4 @@
 /* Ethernet RAVB */
 #define CONFIG_BITBANGMII_MULTI
 
-#endif /* __SMARC_RZG2L_H */
+#endif /* __RZG2LC_SOLIDRUN_H */
