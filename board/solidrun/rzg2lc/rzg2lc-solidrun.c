1f945536d855c206c95fe7efc619800c48ec2b46
Yazan Shhady <yazan.shhady@solid-run.com>
2022-09-27 23:21:45 +0300

    change board name to rzg2lc-solidrun

--- board/solidrun/rzg2lc/rzg2lc-solidrun.c
+++ board/solidrun/rzg2lc/rzg2lc-solidrun.c
@@ -50,7 +50,7 @@ void s_init(void)
 	/* SD1 power control: P39_1 = 0; P39_2 = 1; */
 	*(volatile u32 *)(PFC_PMC37) &= 0xFFFFFFF9; /* Port func mode 0b00 */
 	*(volatile u32 *)(PFC_PM37) = (*(volatile u32 *)(PFC_PM37) & 0xFFFFFFC3) | 0x28; /* Port output mode 0b1010 */
-#if CONFIG_TARGET_SMARC_RZG2LC
+#if CONFIG_TARGET_RZG2LC_SOLIDRUN
 	*(volatile u32 *)(PFC_P37) = (*(volatile u32 *)(PFC_P37) & 0xFFFFFFF9) | 0x6;	/* Port 39[2:1] output value 0b11*/
 #else
 	*(volatile u32 *)(PFC_P37) = (*(volatile u32 *)(PFC_P37) & 0xFFFFFFF9) | 0x4;	/* Port 39[2:1] output value 0b10*/
