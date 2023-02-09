a1dbebe1415ef2ab8982a736842acab466c6a48d
Narinder Dhillon <ndhillon@marvell.com>
2023-02-08 18:57:05 -0800

    efi: Fix SNP test log
    This commit fixes interface log for snp test.
    
    Signed-off-by: Narinder Dhillon <ndhillon@marvell.com>
    
    Change-Id: I9cc1e3567aa19beaaddbb55b699403dc5b8dda60
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/u-boot/+/96465
    Reviewed-by: Suneel Garapati <sgarapati@caviumnetworks.com>
    Tested-by: Suneel Garapati <sgarapati@caviumnetworks.com>

--- lib/efi_selftest/efi_selftest_snp.c
+++ lib/efi_selftest/efi_selftest_snp.c
@@ -370,7 +370,7 @@ static int execute(void)
 			continue;
 		}
 
-		efi_st_printf("Testing eth%ld\n", i);
+		efi_st_printf("Testing eth%d\n", (unsigned int)i);
 		/*
 		 * Check hardware address size.
 		 */
