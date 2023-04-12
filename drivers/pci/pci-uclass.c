f3f6d0911a6e64bfd2f4adf8dd023988a9ec2341
Suneel Garapati <sgarapati@marvell.com>
2023-04-11 18:32:35 -0700

    drivers: pci: Check for valid device in set_vga_control
    Before setting VGA control in bridge check for validity of
    PCI device.
    
    Change-Id: I9ec3c3c6b424be010f87cfb1dba1bd19dfa487a6
    Signed-off-by: Suneel Garapati <sgarapati@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/u-boot/+/101137
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- drivers/pci/pci-uclass.c
+++ drivers/pci/pci-uclass.c
@@ -527,6 +527,8 @@ static void set_vga_bridge_bits(struct udevice *dev)
 	u16 bc;
 
 	while (dev_seq(parent) != 0) {
+		if (!dev_get_parent_plat(parent))
+			return;
 		dm_pci_read_config16(parent, PCI_BRIDGE_CONTROL, &bc);
 		bc |= PCI_BRIDGE_CTL_VGA;
 		dm_pci_write_config16(parent, PCI_BRIDGE_CONTROL, bc);
