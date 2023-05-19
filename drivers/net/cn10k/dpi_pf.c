0b1425f92a828ff482db2b18f3aabac7eba57000
Suneel Garapati <sgarapati@marvell.com>
2023-05-19 09:00:30 -0700

    drivers: net: cn10k: Fix status check logic in dpi_start
    Incorrect check on count to be zero where it is bound to
    go negative if host is not running. Fix it.
    
    Change-Id: I072acff797f1106602c504cd86a4bf4193eddabc
    Signed-off-by: Suneel Garapati <sgarapati@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/u-boot/+/103875
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>

--- drivers/net/cn10k/dpi_pf.c
+++ drivers/net/cn10k/dpi_pf.c
@@ -367,16 +367,20 @@ int dpi_xmit(struct udevice *dev, void *pkt, int pkt_len)
 	mb_check_msg(dpi);
 
 	if (read_bar4_reg(HOST_STATUS_REG) != HOST_RUNNING) {
-		printf("%s Host not running\n", __func__);
+		debug("%s Host not running\n", __func__);
 		return -1;
 	}
 	tq = &dpi->txqs;
 	descq = tq->hw_descq;
+	if (!descq)
+		return -1;
 
 	cons_idx = tq->local_cons_idx;
 	prod_idx = descq->prod_idx;
 
 	ptr = &descq->desc_arr[cons_idx];
+	if (!ptr)
+		return -1;
 	ptr->hdr.s_mgmt_net.total_len = pkt_len;
 	ptr->hdr.s_mgmt_net.ptr_len   = pkt_len;
 	ptr->hdr.s_mgmt_net.is_frag = 0;
@@ -502,11 +506,13 @@ int dpi_recv(struct udevice *dev, int flags, uchar **packetp)
 	mb_check_msg(dpi);
 
 	if (read_bar4_reg(HOST_STATUS_REG) != HOST_RUNNING) {
-		printf("%s Host not running\n", __func__);
+		debug("%s Host not running\n", __func__);
 		return -1;
 	}
 	rq = &dpi->rxqs;
 	descq = rq->hw_descq;
+	if (!descq)
+		return -1;
 
 	cons_idx = rq->local_cons_idx;
 	prod_idx = descq->prod_idx;
@@ -517,6 +523,8 @@ int dpi_recv(struct udevice *dev, int flags, uchar **packetp)
 
 	ptr = &descq->desc_arr[cons_idx];
 	pkt_buf = (void *)rq->dma_list[cons_idx];
+	if (!ptr || !pkt_buf)
+		return -1;
 
 	instr.dma_len = ptr->hdr.s_mgmt_net.total_len;
 	instr.hostaddr = ptr->ptr;
@@ -598,7 +606,7 @@ int dpi_start(struct udevice *dev)
 		if (read_bar4_reg(HOST_STATUS_REG) == HOST_RUNNING)
 			break;
 	}
-	if (!count && (read_bar4_reg(HOST_STATUS_REG) != HOST_RUNNING)) {
+	if (count < 0 && (read_bar4_reg(HOST_STATUS_REG) != HOST_RUNNING)) {
 		printf("%s Host not running\n", __func__);
 		return -1;
 	}
