8d298bb7eb1c1e860d862f5ad8d7e49cabb0b2a3
Jon Nettleton <jon@solid-run.com>
2023-05-29 05:48:56 +0200

    solidrun: imx8mp: unify TLV parsing and storage
    All SolidRun devices use the same TLV version and information.
    Rather than splitting this out and duplicating code for each
    board, do all the information parsing into a common function
    that all boards can share. Then each board can parse the data
    that it needs to initialize itself.
    
    Signed-off-by: Jon Nettleton <jon@solid-run.com>

--- board/solidrun/common/tlv_data.c
+++ board/solidrun/common/tlv_data.c
@@ -9,31 +9,54 @@
 #include <tlv_eeprom.h>
 #include "tlv_data.h"
 
-#define SR_TLV_CODE_RAM_SIZE	0x81
-
 static void store_product_name(struct tlvinfo_tlv *tlv_entry,
-			       struct tlv_data *td)
+			       struct tlv_data *td, int index)
 {
 	int len;
 	char *dest;
 
-	if (strlen(td->tlv_product_name[0]) == 0)
-		dest = td->tlv_product_name[0];
-	else if (strlen(td->tlv_product_name[1]) == 0)
-		dest = td->tlv_product_name[1];
-	else
-		return;
-
+	dest = td->tlv_product_name[index];
 	len = min_t(unsigned int, tlv_entry->length,
-		    sizeof(td->tlv_product_name[0]) - 1);
+		    sizeof(td->tlv_product_name[index]) - 1);
 	memcpy(dest, tlv_entry->value, len);
 }
 
+static void store_part_number(struct tlvinfo_tlv *tlv_entry,
+			      struct tlv_data *td, int index)
+{
+	int len;
+	char *dest;
+
+	dest = td->tlv_part_number[index];
+	len = min_t(unsigned int, tlv_entry->length,
+		    sizeof(td->tlv_part_number[index]) - 1);
+	memcpy(dest, tlv_entry->value, len);
+}
+
+static void store_mac_size(struct tlvinfo_tlv *tlv_entry,
+			   struct tlv_data *td, int index)
+{
+	td->tlv_mac_count[index] = (tlv_entry->value[0] << 8) | tlv_entry->value[1];
+	pr_debug("%s: read mac count = %u\n", __func__, td->tlv_mac_count[index]);
+}
+
+static void store_mac_base(struct tlvinfo_tlv *tlv_entry,
+			   struct tlv_data *td, int index)
+{
+	char *dest;
+
+	dest = td->tlv_mac_base[index];
+	memcpy(dest, tlv_entry->value, 6);
+	pr_debug("%s: read mac base = %02X:%02X:%02X:%02X:%02X:%02X\n", __func__, dest[0], dest[1], dest[2], dest[3], dest[4], dest[5]);
+}
+
 static void parse_tlv_vendor_ext(struct tlvinfo_tlv *tlv_entry,
-				 struct tlv_data *td)
+				 struct tlv_data *td, int index)
 {
 	u8 *val = tlv_entry->value;
 	u32 pen; /* IANA Private Enterprise Numbers */
+	int len;
+	char *dest;
 
 	if (tlv_entry->length < 5) /* 4 bytes PEN + at least 1 byte type */
 		return;
@@ -44,30 +67,56 @@ static void parse_tlv_vendor_ext(struct tlvinfo_tlv *tlv_entry,
 	if (pen != 0xffffffff)
 		return;
 
-	if (val[4] != SR_TLV_CODE_RAM_SIZE)
-		return;
-	if (tlv_entry->length < 6)
-		return;
-	td->ram_size = val[5];
+	switch (val[4]) {
+	case SR_TLV_CODE_RAM_SIZE:
+		if (tlv_entry->length != 6)
+			break;
+		td->ram_size = val[5];
 
-	/* extension with additional data field for number of ddr channels */
-	if (tlv_entry->length >= 7) {
-		td->ram_channels = val[6];
-	}
+		/* extension with additional data field for number of ddr channels */
+		if (tlv_entry->length >= 7) {
+			td->ram_channels = val[6];
+		}
+		break;
+	case SR_TLV_CODE_KIT_NUMBER:
+		if (tlv_entry->length > 257)
+			break;
+		dest = td->tlv_kit_number[index];
+		len = min_t(unsigned int, tlv_entry->length - 5,
+			    sizeof(td->tlv_kit_number[index]) - 1);
+		memcpy(dest, &tlv_entry->value[5], len);
+		break;
+	default:
+		break;
+	};
+
+	return;
 }
 
 static void parse_tlv_data(u8 *eeprom, struct tlvinfo_priv *tlv,
-						   struct tlv_data *td)
+			   struct tlv_data *td, int index)
 {
 	struct tlvinfo_tlv *entry;
 
 	entry = tlv_entry_next_by_code(tlv, NULL, TLV_CODE_PRODUCT_NAME);
 	if (!IS_ERR(entry))
-		store_product_name(entry, td);
+		store_product_name(entry, td, index);
+
+	entry = tlv_entry_next_by_code(tlv, NULL, TLV_CODE_PART_NUMBER);
+	if (!IS_ERR(entry))
+		store_part_number(entry, td, index);
+
+	entry = tlv_entry_next_by_code(tlv, NULL, TLV_CODE_MAC_SIZE);
+	if (!IS_ERR(entry))
+		store_mac_size(entry, td, index);
+
+	entry = tlv_entry_next_by_code(tlv, NULL, TLV_CODE_MAC_BASE);
+	if (!IS_ERR(entry))
+		store_mac_base(entry, td, index);
 
 	entry = tlv_entry_next_by_code(tlv, NULL, TLV_CODE_VENDOR_EXT);
 	if (!IS_ERR(entry))
-		parse_tlv_vendor_ext(entry, td);
+		parse_tlv_vendor_ext(entry, td, index);
 }
 
 void read_tlv_data(struct tlv_data *td)
@@ -76,11 +125,11 @@ void read_tlv_data(struct tlv_data *td)
 	struct tlvinfo_priv *priv;
 	int i;
 
-	for (i = 0; i < 2; i++) {
+	for (i = 0; i < TLV_MAX_DEVICES; i++) {
 		priv = tlv_eeprom_read(tlv_eeprom_get_by_index(i), 0, eeprom_data, ARRAY_SIZE(eeprom_data));
 		if (IS_ERR(priv))
 			continue;
-		parse_tlv_data(eeprom_data, priv, td);
+		parse_tlv_data(eeprom_data, priv, td, i);
 	}
 }
 
--- board/solidrun/common/tlv_data.h
+++ board/solidrun/common/tlv_data.h
@@ -6,11 +6,33 @@
 #ifndef __BOARD_SR_COMMON_H_
 #define __BOARD_SR_COMMON_H_
 
+#define TLV_MAX_DEVICES			2
+
 struct tlv_data {
 	/* Store product name of both SOM and carrier */
-	char tlv_product_name[2][32];
+	char tlv_product_name[TLV_MAX_DEVICES][32];
+	char tlv_part_number[TLV_MAX_DEVICES][257];
+	char tlv_kit_number[TLV_MAX_DEVICES][257];
+	unsigned char tlv_mac_base[TLV_MAX_DEVICES][6];
+	u16 tlv_mac_count[TLV_MAX_DEVICES];
 	unsigned int ram_size;
 	uint8_t ram_channels;
+};
+
+/*
+ * SolidRun TLV vendor extension data format
+ * (compatible with format used on Armada 388 Clearfog by Baruch Siach)
+ */
+struct __attribute__((__packed__)) sr_tlv_ext {
+	u32 pen; // IANA Private Enterprise Number
+	u8 code; // identification code for data
+	u8 data[];
+};
+
+// SolidRun TLV vendor extension data codes
+enum sr_tlv_code {
+	SR_TLV_CODE_KIT_NUMBER  = 0x10,
+	SR_TLV_CODE_RAM_SIZE    = 0x81,
 };
 
 void read_tlv_data(struct tlv_data *td);
