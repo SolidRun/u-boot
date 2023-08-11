3ea59b30934e52185c1b482b5e85b3b101e8ab1c
Paul Way <pway@marvell.com>
2023-08-11 11:12:16 -0600

    Revert "Add ability to have linux kernel written from ATF."
    This reverts commit 636f0288953ed889d611e5dee0acbba1c69a4419.
    
    Signed-off-by: Paul Way <pway@marvell.com>
    Change-Id: I04c08f9d7a91126748c0fa827e4c165202f56944
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/u-boot/+/109713
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- common/bootm.c
+++ common/bootm.c
@@ -245,6 +245,7 @@ int bootm_find_images(int flag, int argc, char *const argv[], ulong start,
 		      ulong size)
 {
 	int ret;
+
 	/* find ramdisk */
 	ret = boot_get_ramdisk(argc, argv, &images, IH_INITRD_ARCH,
 			       &images.rd_start, &images.rd_end);
@@ -286,6 +287,7 @@ int bootm_find_images(int flag, int argc, char *const argv[], ulong start,
 	if (CONFIG_IS_ENABLED(CMD_FDT))
 		set_working_fdt_addr(map_to_sysmem(images.ft_addr));
 #endif
+
 #if IMAGE_ENABLE_FIT
 #if defined(CONFIG_FPGA)
 	/* find bitstreams */
@@ -603,7 +605,6 @@ int do_bootm_states(struct cmd_tbl *cmdtp, int flag, int argc,
 	/* From now on, we need the OS boot function */
 	if (ret)
 		return ret;
-
 	boot_fn = bootm_os_get_boot_func(images->os.os);
 	need_boot_fn = states & (BOOTM_STATE_OS_CMDLINE |
 			BOOTM_STATE_OS_BD_T | BOOTM_STATE_OS_PREP |
