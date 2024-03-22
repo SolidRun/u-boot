f67532c8589287a66129a4bd753ac9252a986fb2
Aaron Williams <awilliams@marvell.com>
2024-03-21 19:19:04 -0700

    SoC is ARMv9-a
    This change causes the compiler to use an updated instruction set which
    helps in the case of atomic operations needed by the PCIe console driver.
    This also will tune the compiler to the appropriate ARM core.  Note that
    this also assumes that gcc-12 or later is being used.
    
    Change-Id: I7cef8a4277db404f274a8bf1a951dca2735d6b90
    Signed-off-by: Aaron Williams <awilliams@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/boot/u-boot/+/124926
    Reviewed-by: Narinder Dhillon <ndhillon@marvell.com>
    Reviewed-by: Chandrakala Chavva <cchavva@marvell.com>
    Tested-by: Chandrakala Chavva <cchavva@marvell.com>

--- arch/arm/mach-cn10k/config.mk
+++ arch/arm/mach-cn10k/config.mk
@@ -1,3 +1,4 @@
 ifeq ($(CONFIG_ARCH_CN10K),y)
-PLATFORM_CPPFLAGS += $(call cc-option,-march=armv8.2-a,)
+PLATFORM_CPPFLAGS += $(call cc-option,-march=armv9-a,)
+PLATFORM_CPPFLAGS += $(call cc-option,-mtune=neoverse-n2)
 endif
