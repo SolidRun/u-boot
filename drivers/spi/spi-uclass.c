MarvellEmbeddedProcessors/u-boot-marvell

spi: set spi speed to the minimum between controller's and flash's frequency
Both spi controller and flash have got the maximum working freq, the
correct freq should be the lower one of these two.
Now there is a bug that when flash's freq is higher than controller's,
spi uclass sets the working freq to flash's, instead of controller's.
This patch sets spi speed to the minimum between controller's and
flash's frequency.

Change-Id: If337f65a0be2146cc15279810c8edefb2a2eb7d2
Signed-off-by: Ken Ma <make@marvell.com>
Reviewed-on: http://vgitil04.il.marvell.com:8080/55226
Reviewed-by: Igal Liberman <igall@marvell.com>
Tested-by: Igal Liberman <igall@marvell.com>

drivers/spi/spi-uclass.c
@@ -271,6 +271,7 @@ int spi_get_bus_and_cs(int busnum, int cs, int speed, int mode,
 {
 	struct udevice *bus, *dev;
 	struct dm_spi_slave_platdata *plat;
+	struct dm_spi_bus *spi;
 	bool created = false;
 	int ret;
 
@@ -284,6 +285,7 @@ int spi_get_bus_and_cs(int busnum, int cs, int speed, int mode,
 		return ret;
 	}
 	ret = spi_find_chip_select(bus, cs, &dev);
+	spi = dev_get_uclass_priv(bus);
 
 	/*
 	 * If there is no such device, create one automatically. This means
@@ -325,6 +327,9 @@ int spi_get_bus_and_cs(int busnum, int cs, int speed, int mode,
 		speed = plat->max_hz;
 		mode = plat->mode;
 	}
+	if (spi->max_hz)
+		speed = min(speed, (int)spi->max_hz);
+
 	ret = spi_set_speed_mode(bus, speed, mode);
 	if (ret)
 		goto err;

SolidRun/u-boot

Change-Id: Ibb9c54795cd7022fd0f8106f0c16cb976fd97266

drivers/spi/spi-uclass.c
@@ -338,6 +338,7 @@ int spi_get_bus_and_cs(int busnum, int cs, int speed, int mode,
 		return ret;
 	}
 	ret = spi_find_chip_select(bus, cs, &dev);
+	spi = dev_get_uclass_priv(bus);
 
 	/*
 	 * If there is no such device, create one automatically. This means
@@ -391,6 +392,13 @@ int spi_get_bus_and_cs(int busnum, int cs, int speed, int mode,
 		if (ret)
 			goto err;
 	}
+
+	if (slave->max_hz)
+		speed = min(speed, (int)slave->max_hz);
+
+	ret = spi_set_speed_mode(bus, speed, mode);
+	if (ret)
+		goto err;
 
 	*busp = bus;
 	*devp = slave;